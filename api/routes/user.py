23e3b7231f0705999d14284ad115a4b642c2935e
reardek <jjkowalinski@yahoo.com>
2021-11-30 21:34:06 +0100

    Rejecting existing user

--- api/routes/user.py
+++ api/routes/user.py
@@ -1,5 +1,6 @@
 from fastapi import Depends
 from fastapi.routing import APIRouter
+from fastapi.exceptions import HTTPException
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from starlette import status
 from api.dependencies.db import get_db
@@ -14,5 +15,8 @@ user_router = APIRouter(prefix="/user")
 @user_router.post("/register", status_code=status.HTTP_201_CREATED, response_model=UserToken)
 async def create_user(payload: InUserSchema, session: AsyncSession = Depends(get_db)) -> UserToken:
     user_repository = UsersRepository(session)
-    token = await user_repository.create(payload)
+    try:
+        token = await user_repository.create_or_reject(payload)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     return UserToken(token="token")
--- database/repositories/user.py
+++ database/repositories/user.py
@@ -1,11 +1,14 @@
 from typing import Type
 from uuid import uuid4
+from logging import log
 
 from pydantic.main import BaseModel
 from database.models.users import User
 
 from database.repositories.base import BaseRepository
 from schema.user import InUserSchema, OutUserSchema
+
+from sqlalchemy import select
 
 
 class UserToken(BaseModel):
@@ -25,3 +28,9 @@ class UsersRepository(BaseRepository[InUserSchema, OutUserSchema, User]):
     def _table(self) -> Type[User]:
         return User
 
+    async def create_or_reject(self, in_user: InUserSchema):
+        user_query = select(User).where(User.email == in_user.email)
+        user = await self._db_session.execute(user_query)
+        if user.fetchall():
+            raise Exception(f"User with email {in_user.email} already exists")
+        await self.create(in_user)
